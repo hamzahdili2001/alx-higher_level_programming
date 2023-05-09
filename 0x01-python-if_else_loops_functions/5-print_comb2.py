d2b1ed3008a0b24a5a93623b7418d2e9f069e373
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-09 10:18:13 +0100

    Fix-Change: f to format()

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(f"{i:02d}", end=', ')
+        print("{:02d}".format(i), end=', ')
     elif i != 99 and i >= 10:
-        print(f"{i:d}", end=", ")
+        print("{:d}".format(i), end=", ")
     else:
-        print(f"{i:d}")
+        print("{:d}".format(i))
