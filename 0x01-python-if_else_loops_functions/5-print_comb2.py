bbe82b022a0ba3a07af6f3feab4b97fc7fffba79
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-09 10:21:56 +0100

    Fix-Change: f to format() and remove one print from the code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
+    if i != 99:
         print("{:02d}".format(i), end=', ')
-    elif i != 99 and i >= 10:
-        print("{:d}".format(i), end=", ")
     else:
-        print("{:d}".format(i))
+        print(i)
