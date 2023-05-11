b5a5aaa4611dce1e6d1e4cc48f75ce3b9db72db0
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-11 10:09:41 +0100

    Fix: should not be executed when imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-add_ = add(a, b)
-sub_ = sub(a, b)
-mul_ = mul(a, b)
-div_ = div(a, b)
+    add_ = add(a, b)
+    sub_ = sub(a, b)
+    mul_ = mul(a, b)
+    div_ = div(a, b)
 
-print("{:d} + {:d} = {:d}".format(a, b, add_))
-print("{:d} - {:d} = {:d}".format(a, b, sub_))
-print("{:d} * {:d} = {:d}".format(a, b, mul_))
-print("{:d} / {:d} = {:d}".format(a, b, div_))
+    print("{:d} + {:d} = {:d}".format(a, b, add_))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_))
+    print("{:d} * {:d} = {:d}".format(a, b, mul_))
+    print("{:d} / {:d} = {:d}".format(a, b, div_))
