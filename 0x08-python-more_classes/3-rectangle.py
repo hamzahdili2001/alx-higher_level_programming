823b493d7325872c7789db2fb1bf79b5c0474c7b
hamzahdili2001 <hdilihamza955@gmail.com>
2023-06-05 15:47:33 +0100

    Task 3: Done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,8 +56,9 @@ class Rectangle():
         """Print Rectangle with char '#'."""
         if self.__width == 0 or self.__height == 0:
             return ""
+        rows = ['#' * self.__width for _ in range(self.__height)]
 
-        return "\n".join(['#' * self.__width] * self.__height)
+        return "\n".join(rows)
 
     def __repr__(self):
         """
