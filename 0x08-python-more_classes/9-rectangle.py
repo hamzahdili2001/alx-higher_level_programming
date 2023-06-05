cccaa0b51ca78538aa944c77fad6e586d7f7d1a4
hamzahdili2001 <hdilihamza955@gmail.com>
2023-06-05 17:06:41 +0100

    Task 9: Done

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return ''
         return '\n'.join(
-            [str(Rectangle.print_symbol) * self.__width]
+            [str(self.print_symbol) * self.__width]
             * self.__height)
 
     def __repr__(self):
