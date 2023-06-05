a0cf592428ee43a189ffcc1613b709a88a02bd67
hamzahdili2001 <hdilihamza955@gmail.com>
2023-06-05 16:51:03 +0100

    Task 7, 8: Done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return ''
         return '\n'.join(
-            [str(self.print_symbol) * self.__width]
+            [str(Rectangle.print_symbol) * self.__width]
             * self.__height)
 
     def __repr__(self):
@@ -74,3 +74,12 @@ class Rectangle():
         """Prints Message when an instance deleted"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Static Method"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
