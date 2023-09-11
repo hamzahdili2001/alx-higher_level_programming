2f184c1420950a50effc2aa61e85edc9639a06ff
hamzahdili2001 <hdilihamza955@gmail.com>
2023-09-11 14:53:27 +0100

    Add: Task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""unction that returns True if the object is
-an instance of a class that inherited
-(directly or indirectly)
-from the specified class ; otherwise False."""
+"""Function that returns True if the object is
+an instance of a class that inherited"""
 
 
 def inherits_from(obj, a_class):
-    """Returns True or False"""
+    """Returns True or false"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
