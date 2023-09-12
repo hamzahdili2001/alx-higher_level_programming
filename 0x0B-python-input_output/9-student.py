3d0d36695b2793a80f699af0b66e25a00da24ba9
hamzahdili2001 <hdilihamza955@gmail.com>
2023-09-12 17:37:54 +0100

    add: Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """Student instances"""
+
+    def __init__(self) -> None:
+        """constractor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dir descriptor"""
+        return self.__dict__.copy()
