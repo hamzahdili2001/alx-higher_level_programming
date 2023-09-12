b1a6cc703005600fe342a679cb29b1b4a72eee76
hamzahdili2001 <hdilihamza955@gmail.com>
2023-09-12 17:39:28 +0100

    fix: Task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,7 @@
 class Student:
     """Student instances"""
 
-    def __init__(self) -> None:
+    def __init__(self, first_name, last_name, age) -> None:
         """constractor"""
         self.first_name = first_name
         self.last_name = last_name
