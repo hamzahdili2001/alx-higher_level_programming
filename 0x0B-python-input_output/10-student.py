5eb3ebbf76ff6d8b3cb6b4d09c5b751ba75093ab
hamzahdili2001 <hdilihamza955@gmail.com>
2023-09-12 17:48:09 +0100

    fix: Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,22 +5,22 @@
 class Student:
     """Student instances"""
 
-    def __init__(self, first_name, last_name, age) -> None:
+    def __init__(self, first_name, last_name, age):
         """constractor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
+    def to_json(self, attrs=None):
         """Function that retrieves a dictionary
         representation of a Student instance
         (same as 8-class_to_json.py)"""
 
-        if attr is None:
+        if attrs is None:
             j_dict = self.__dict__
         else:
             j_dict = {}
-            for a in attr:
-                if hasattr(self, attr):
+            for a in attrs:
+                if hasattr(self, a):
                     j_dict[a] = getattr(self, a)
         return j_dict
