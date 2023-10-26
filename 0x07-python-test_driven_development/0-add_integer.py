c306ddc9f09f74a100cb15d7ff2e3e11104a2a03
hamzahdili2001 <hdilihamza955@gmail.com>
2023-10-26 18:33:30 +0100

    [Fix] 0-add_integer.py -docs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-'''Function that adds two integer'''
+'''
+Function that adds two integer
+Usage:
+    add_integer(number_one, number_two)
+'''
 
 
 def add_integer(a, b=98):
-    '''Function that adds two integer
-    Return: result of two number
     '''
+    Function that adds two integer
+    Return: result of two number'''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
