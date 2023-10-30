6d2fa2312a65f5d5320558f3f80e8aad141a6964
hamzahdili2001 <hdilihamza955@gmail.com>
2023-10-30 16:39:31 +0100

    [Add] 9-rectangle.py  -fixed

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """initialize private instances attribute"""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
