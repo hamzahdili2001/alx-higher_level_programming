3f560e64a5e7624934fe36c4eb40c1094219f11b
hamzahdili2001 <hdilihamza955@gmail.com>
2023-10-30 15:36:19 +0100

    [Add] 0-rectangle.py 1-rectangle.py  -done -working

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""0-Rectangle.py"""
+
+
+class Rectangle:
+    """empty class Rectangle that defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""1-Rectangle.py"""
+
+
+class Rectangle:
+    """class that defines a rectangle by: (based on 0-rectangle.py)"""
+
+    def __init__(self, width=0, height=0):
+        """initialize private instances attribute"""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the value to the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the value to the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
