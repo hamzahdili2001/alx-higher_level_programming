d6c1326e716d422cbe4a0c68a5dfafdb4e83121a
hamzahdili2001 <hdilihamza955@gmail.com>
2023-11-07 21:05:28 +0100

    [init] files -done [add] 0-read_file.py -done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-read_file.py"""
+
+
+def read_file(filename=""):
+    """read from a file"""
+    with open(filename, "r", encoding="UTF-8") as file:
+        for line in file:
+            print(line)
