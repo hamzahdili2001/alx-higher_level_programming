bc3eab62fa964a694b930e653b6436d6c24c81b5
hamzahdili2001 <hdilihamza955@gmail.com>
2023-11-07 23:58:35 +0100

    [add] 101-stats.py -done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
-"""101-stats.py"""
-
 
 import sys
 
 totalFileSize = 0
 statusCodeCounts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
 }
 lineCount = 0
+
+
+def print_info():
+    print("File size: {}".format(totalFileSize))
+    for code in sorted(statusCodeCounts.keys()):
+        if statusCodeCounts[code] > 0:
+            print("{}: {}".format(code, statusCodeCounts[code]))
+
+
+def add_status_code(status):
+    if status in statusCodeCounts:
+        statusCodeCounts[status] += 1
+
 
 try:
     for line in sys.stdin:
         parts = line.split()
         if len(parts) >= 9:
-            statusCode = int(parts[-2])
+            statusCode = parts[-2]
             fileSize = int(parts[-1])
 
             totalFileSize += fileSize
-            statusCodeCounts[statusCode] += 1
+            add_status_code(statusCode)
             lineCount += 1
 
         if lineCount % 10 == 0:
-            print("File size: {}".format(totalFileSize))
-            for code in sorted(statusCodeCounts.keys()):
-                if statusCodeCounts[code] > 0:
-                    print("{}: {}".format(code, statusCodeCounts[code]))
+            print_info()
 
 except KeyboardInterrupt:
-    pass
-
-print("File size: {}".format(totalFileSize))
-for code in sorted(statusCodeCounts.keys()):
-    if statusCodeCounts[code] > 0:
-        print("{}: {}".format(code, statusCodeCounts[code]))
+    print_info()
+    raise
