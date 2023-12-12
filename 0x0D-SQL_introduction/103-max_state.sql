36601e38a03ac748317b707f50ffc125aeb89574
hamzahdili2001 <hdilihamza955@gmail.com>
2023-12-12 21:53:17 +0100

    [fix] task-20 (did task 19 again in 20)

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,5 @@
 -- script that displays the max temperature of each state (ordered by State name).
-SELECT city, AVG(value) AS avg_temp
+SELECT state, max(value) as max_temp
 FROM temperatures
-WHERE month IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+GROUP BY state
+ORDER by state;
