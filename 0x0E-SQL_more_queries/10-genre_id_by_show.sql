5f40cf59edea7999d4454d0d24ee02fe10c41e6c
hamzahdili2001 <hdilihamza955@gmail.com>
2023-12-13 18:17:41 +0100

    [fix] autoformating

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
 -- Lists all the shows contained in the database hbtn_0d_tvshows that have at least one genre linked
--- Can ONLY use one SELECT statement and the results must be sorted in ascending order by:
--- 	tv_shows.title and tv_show_genres.genre_id
--- Database name is passed as an argument of the mysql command
+
 SELECT title, genre_id
 FROM tv_shows
 FULL JOIN tv_show_genres
