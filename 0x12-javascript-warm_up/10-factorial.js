75e5175023bf738b2c8d972d82f3ff429ae4890e
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-08 17:58:20 +0100

    [add] task-10: script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || n === 1 || isNaN(n)) { return 1; } else { return (n * factorial(n - 1)); }
+}
+console.log(factorial(Number.parseInt(process.argv[2])));
