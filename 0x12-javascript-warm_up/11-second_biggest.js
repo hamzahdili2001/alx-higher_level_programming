b275712d2eaec4266e404b7ed89b04113c63cf8a
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-08 18:09:12 +0100

    [fix] task-11:  script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const ints = [];
-const length = process.argv.length - 2;
 if (process.argv.length <= 3) {
   console.log(1);
 } else {
+  const ints = [];
+  const length = process.argv.length - 2;
   for (let i = 2; i < process.argv.length; i++) {
     ints.push(process.argv[i]);
   }
