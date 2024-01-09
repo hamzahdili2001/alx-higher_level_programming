31438fad3ba84db0e75a40dde0109ff07c9e7dad
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-09 18:03:43 +0100

    [add-advanced] task-13: script that concats 2 files.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const fileA = process.argv[2]; const fileB = process.argv[3]; const fileC = process.argv[4];
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 
-try {
-  [fileA, fileB].forEach((f) => {
-    fs.readFileSync(f).toString().trim().split('\n').forEach((line) => {
-      fs.appendFileSync(fileC, line.toString() + '\n');
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+    const concatenatedData = dataA + dataB;
+    fs.writeFile(fileC, concatenatedData, (err) => {
+      if (err) throw err;
     });
   });
-} catch (err) {
-  console.log(err);
-}
+});
