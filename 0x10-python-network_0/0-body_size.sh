ce6ba9c16b2764c454208f5c733a55146fada7aa
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-25 12:23:18 +0100

    [add] task-0:Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# size must be displayed in bytes
+
+curl -sfI "$1" | grep 'Content-Length' | cut -d " " -f2
