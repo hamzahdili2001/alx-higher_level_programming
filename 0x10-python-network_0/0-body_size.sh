84ad6b5a6764f744721d2e0dba217af2ee9dd5af
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-25 12:51:15 +0100

    [fix] script should be 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# size must be displayed in bytes
-
 curl -sfI "$1" | grep 'Content-Length' | cut -d " " -f2
