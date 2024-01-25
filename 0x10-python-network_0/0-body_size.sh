866a60f2d52d4a8b0dee4039d48f98f3b98338f6
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-25 12:38:14 +0100

    [fix] first line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 # size must be displayed in bytes
 
