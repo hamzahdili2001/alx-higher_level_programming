627c74ce58f79237444ac934e5d5ca47ac14e035
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-25 14:37:08 +0100

    [fix] prevent the redirect

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -s 0.0.0.0:5000/catch_me
+curl -s -L 0.0.0.0:5000/catch_me
