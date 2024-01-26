55a2f5a226c2a483f961494e9bdb677edb7609d5
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-26 15:05:51 +0100

    [add] task-10: script that lists 10 commits (from the most recent to oldest)

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that list 10 commits (from the most recent to oldest)"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    github_url = "https://api.github.com/repos/{}/{}/commits".format(
+        repository_name, owner_name
+    )
+    req = requests.get(github_url)
+    commits = req.json()
+    for commit in commits[:10]:
+        commit_sha = commit.get("sha")
+        commit_author_name = (
+            commit.get("commit").get("author").get("name")
+        )
+        print("{}: {}".format(commit_sha, commit_author_name))
