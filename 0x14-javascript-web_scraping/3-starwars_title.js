ec6c2db563cb6332a55e7280e28caff429f48151
hamzahdili2001 <hdilihamza955@gmail.com>
2024-02-20 16:10:55 +0100

    [fix] task-3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,10 +6,9 @@
 
 const request = require('request');
 if (process.argv.length > 2) {
-	request(`https://swapi-api.alx-tools.com/api/films/${parseInt(process.argv[2], 10)}`, (error, response, body) => {
-		if (error) cosole.log(error);
-		console.log(JSON.parse(body).title);
-	}
-	);
+  request(`https://swapi-api.alx-tools.com/api/films/${parseInt(process.argv[2], 10)}`, (error, response, body) => {
+    if (error) console.log(error);
+    console.log(JSON.parse(body).title);
+  }
+  );
 }
-
