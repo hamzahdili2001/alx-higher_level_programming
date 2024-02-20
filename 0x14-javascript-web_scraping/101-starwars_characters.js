49b15301d177acc8e160434f221af1f5b02cce91
hamzahdili2001 <hdilihamza955@gmail.com>
2024-02-20 16:25:00 +0100

    [add] task-8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+if (process.argv.length > 2) {
+  request(`https://swapi-api.alx-tools.com/api/films/${parseInt(process.argv[2], 10)}`, (error, response, body) => {
+    if (error) console.log(error);
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, (error, response, body) => {
+        if (error) console.log(error);
+        console.log(JSON.parse(body).name);
+      });
+    }
+  });
+}
