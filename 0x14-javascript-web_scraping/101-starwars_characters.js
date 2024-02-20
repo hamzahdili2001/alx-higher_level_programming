767ec162eb1e385e4145639cfb08a1328208411a
hamzahdili2001 <hdilihamza955@gmail.com>
2024-02-20 16:28:48 +0100

    [add] task-8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,41 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 if (process.argv.length > 2) {
-  request(`https://swapi-api.alx-tools.com/api/films/${parseInt(process.argv[2], 10)}`, (error, response, body) => {
-    if (error) console.log(error);
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, (error, response, body) => {
-        if (error) console.log(error);
-        console.log(JSON.parse(body).name);
+  const movieId = parseInt(process.argv[2], 10);
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode !== 200) {
+      console.error(`Unexpected status code: ${response.statusCode}`);
+    } else {
+      const filmData = JSON.parse(body);
+      const characters = filmData.characters;
+      const characterPromises = characters.map(characterUrl => {
+        return new Promise((resolve, reject) => {
+          request(characterUrl, (error, response, body) => {
+            if (error) {
+              reject(new Error(error)); // Use Error object as rejection reason
+            } else if (response.statusCode !== 200) {
+              reject(new Error(`Unexpected status code: ${response.statusCode}`)); // Use Error object as rejection reason
+            } else {
+              const characterData = JSON.parse(body);
+              resolve(characterData.name);
+            }
+          });
+        });
       });
+
+      Promise.all(characterPromises)
+        .then(characterNames => {
+          characterNames.forEach(name => console.log(name));
+        })
+        .catch(error => {
+          console.error('Error fetching character data:', error.message);
+        });
     }
   });
 }
